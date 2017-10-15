869465cd5ab85cf715610784781335a951e96670
Jordan Sherman <jordandsherman@gmail.com>
2017-10-15 00:04:27 -0400

    Added homepage markup. Created the register form component and the validation service. Used Angular2 flash messages for validation.

--- angular-cli/src/app/components/register/register.component.ts
+++ angular-cli/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages'
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
 
-  constructor() { }
+  constructor(private validateService: ValidateService, private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    }
+
+    // Required fields
+    if(!this.validateService.validateRegister(user)){
+        this.flashMessage.show('Please fill in all fields.', {cssClass: 'alert-danger', timeout: 3000});
+        return false;
+    }
+
+    if(!this.validateService.validateEmail(user.email)){
+        this.flashMessage.show('Please use a valid email.', {cssClass: 'alert-danger', timeout: 3000});
+        return false;
+    }
   }
 
 }
